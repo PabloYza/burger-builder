2afec8c4482dc4359a65271ff4f7729cec05abe1
PabloYza <P.deyzaguirre13@gmail.com>
2019-10-04 11:03:17 +0200

    Outputting ingredients

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -3,13 +3,19 @@ import classes from './burger.css'
 import BurgerIngredient from './BurgerIngredients/BurguerIngredients';
 
 const burger = (props) => {
+
+  // Object.keys() -> returns an array of the keys from the object
+  const newIngredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />;
+    });
+  });
+  console.log(newIngredients);
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
-      <BurgerIngredient type="salad" />
-      <BurgerIngredient type="meat" />
+      {newIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
