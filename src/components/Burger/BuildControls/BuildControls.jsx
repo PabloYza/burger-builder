60adc64f2b4c75da84ef05cb9717fdcdbc909ed1
PabloYza <P.deyzaguirre13@gmail.com>
2019-10-07 16:44:01 +0200

    Removing ingredients + displaying/updating price

--- src/components/Burger/BuildControls/BuildControls.jsx
+++ src/components/Burger/BuildControls/BuildControls.jsx
@@ -3,19 +3,22 @@ import classes from './BuildControls.css';
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-  {label: 'Salad', type: 'salad'},
-  { label: 'Bacon', type: 'bacon'},
-  {label: 'Cheese', type: 'cheese'},
-  {label: 'Meat', type: 'meat'}
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
 ];
 
 const buildControls = props => (
   <div className={classes.BuildControls}>
+    <p>Current Price: <strong>{props.price.toFixed(2)}€</strong></p>
     {controls.map(control => (
       <BuildControl
         key={control.label}
         label={control.label}
         added={() => props.ingredientAdded(control.type)}
+        removed={() => props.ingredientRemoved(control.type)}
+        disabled={props.disabled[control.type]}
       />
     ))}
   </div>
