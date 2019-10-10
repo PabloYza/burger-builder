76802af561b23eda1b32620e27da97344664e2a8
PabloYza <P.deyzaguirre13@gmail.com>
2019-10-10 17:04:14 +0200

    Backdrop functionality

--- src/components/Navigation/SideDrawer/SideDrawer.jsx
+++ src/components/Navigation/SideDrawer/SideDrawer.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import classes from './SideDrawer.css';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
   // conditionally adapt CSS classes
+  let attachedClasses = [classes.sideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open]
+  }
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Aux>
+      <Backdrop
+        show={props.open}
+        clicked={props.closed}
+      />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 }
 
