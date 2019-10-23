bba7c59eda08078c9bb75b497883acb6aa0434a4
PabloYza <P.deyzaguirre13@gmail.com>
2019-10-23 17:55:27 +0200

    Setting up Routing

--- src/components/Order/CheckoutSummary/CheckoutSummary.jsx
+++ src/components/Order/CheckoutSummary/CheckoutSummary.jsx
@@ -12,11 +12,11 @@ const checkoutSummary = (props) => {
       </div>
       <Button
         btnType='Danger'
-        clicked
+        clicked={props.checkoutCanceled}
       >CANCEL</Button>
       <Button
         btnType='Success'
-        clicked
+        clicked={props.checkoutContinued}
       >CONTINUE</Button>
     </div>
   );
