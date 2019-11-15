5ce0ab7cf1d045f970ff6d74cd447ee010f88c09
PabloYza <P.deyzaguirre13@gmail.com>
2019-11-15 09:17:15 +0100

    setting up Action Creators

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,4 @@
-import * as actionTypes from './actions';
+import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
   ingredients: {
@@ -21,7 +21,7 @@ const INGREDIENT_PRICES = {
 
 const reducer = (state = initialState, action) => {
   switch(action.type) {
-    case actionTypes.ADD_INGREDIENTS:
+    case actionTypes.ADD_INGREDIENT:
       return {
         ...state,
         ingredients: {
