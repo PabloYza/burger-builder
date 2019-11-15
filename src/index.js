f14b8f3d34d47786fae36671c8c53a4ca9c19e88
PabloYza <P.deyzaguirre13@gmail.com>
2019-11-15 09:24:38 +0100

    Applying middleware: thunk

--- src/index.js
+++ src/index.js
@@ -6,13 +6,18 @@ import registerServiceWorker from './registerServiceWorker';
 
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import burgerBuilder from './store/reducers/burgerBuilder';
 
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   burgerBuilder,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
 );
 
 const app = (
