21e0465a5db96f301a55dfcb030da4854950f9e9
PabloYza <P.deyzaguirre13@gmail.com>
2019-11-18 12:32:47 +0100

    Creating the Order reducer

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -12,10 +12,6 @@ import axios from '../../axios-orders';
 import * as burgerBuilderActions from '../../store/actions/index';
 
 class BurgerBuilder extends Component {
-  // constructor(props) {
-  //     super(props);
-  //     this.state = {...}
-  // }
   state = {
     purchasing: false,
   }
